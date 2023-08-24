a6d70c41cc237a3bdb6c01d7ce6fd553ed8a5117
Eraldo Pereira Marinho <32465957+epmarinho@users.noreply.github.com>
2023-08-24 15:57:32 -0300

    Update dosimula.bash
    Fixed parameter list for dosimula

--- src/dosimula.bash
+++ src/dosimula.bash
@@ -3,9 +3,11 @@ simcode=$1
 seriesname=$2
 m=$3
 n=$4
-omega=$5
-epsilon=$6
-dt=$7
+N=$5
+K=$6
+omega=$7
+epsilon=$8
+dt=$9
 if [ -e $seriesname$m.data.gz ]
 then
 	gunzip $seriesname$m.data.gz
@@ -14,7 +16,7 @@ fi
 for((i=m;i<n;i++))
 do
 	let j=i+1
-	time $simcode $seriesname$i.data $omega $epsilon $dt >$seriesname$j.data
+	time $simcode $seriesname$i.data $N $K $omega $epsilon $dt >$seriesname$j.data
 	echo $i '->' $j
 	[ -z $gzflag ] || gzip $seriesname$i.data
 done
